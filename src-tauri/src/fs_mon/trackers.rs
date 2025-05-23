2267ec06efbccb4d96d08a885134d437773112cb
mrVGM <maddvvas@gmail.com>
2025-05-23 15:53:08 +0300

    dir trackers

--- src-tauri/src/fs_mon/trackers.rs
+++ src-tauri/src/fs_mon/trackers.rs
@@ -1,14 +1,19 @@
-use std::{collections::HashMap, sync::{Arc, LazyLock, Mutex}};
+use std::{collections::HashMap, str::FromStr, sync::{Arc, LazyLock, Mutex, Weak}};
 
 use super::file_tag::tag_file;
 
 type FileTrackersMap = HashMap<String, FileTracker>;
+type DirTrackersMap = HashMap<String, Weak<DirTracker>>;
 
 static FILE_TRACKERS: LazyLock<Mutex<FileTrackersMap>> = LazyLock::new(|| -> Mutex<FileTrackersMap> {
     Mutex::new(FileTrackersMap::new())
 });
 
-struct DirTracker;
+static DIR_TRACKERS: LazyLock<Mutex<DirTrackersMap>> = LazyLock::new(|| -> Mutex<DirTrackersMap> {
+    Mutex::new(DirTrackersMap::new())
+});
+
+struct DirTracker(String);
 struct FileTracker {
     dir_tracker: Arc<DirTracker>,
     tracker_state: FileTrackerState
@@ -28,6 +33,14 @@ pub enum FileTrackerState {
     }
 }
 
+fn get_volume_label(file: &str) -> String {
+    let mut file = Some(std::path::PathBuf::from_str(file).unwrap()).unwrap();
+    while let Some(parent) = file.parent() {
+        file = parent.to_path_buf();
+    }
+    file.to_str().unwrap().into()
+}
+
 pub fn register_file(file: &str) -> String {
     let trackers = &mut *(*FILE_TRACKERS).lock().unwrap();
     let file_id = tag_file(file);
@@ -37,8 +50,28 @@ pub fn register_file(file: &str) -> String {
         path: file.into()
     };
 
+    let volume = get_volume_label(file);
+
+    let dir_tracker = 'retrieve_tracker: {
+        let dir_trackers = &mut *DIR_TRACKERS.lock().unwrap();
+        let tracker = dir_trackers.get(&volume);
+
+        if let Some(tracker) = tracker {
+            let real_tracker = tracker.upgrade();
+            if let Some(tracker) = real_tracker {
+                break 'retrieve_tracker tracker;
+            }
+        }
+
+        let dir_tracker = Arc::new(DirTracker(volume.to_owned()));
+        let weak_tracker = Arc::downgrade(&dir_tracker);
+        dir_trackers.insert(volume, weak_tracker);
+
+        dir_tracker
+    };
+
     let tracker = FileTracker {
-        dir_tracker: DirTracker.into(),
+        dir_tracker,
         tracker_state: state
     };
 
